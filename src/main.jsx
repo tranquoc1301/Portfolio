4bd389ecc250c9e22477d448f326179d2f24c7b0
Tran Quoc <tranquoc1301@gmail.com>
2025-07-09 21:08:53 +0700

    add SpeedInsight

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./style/style.scss";
 import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>
 );
